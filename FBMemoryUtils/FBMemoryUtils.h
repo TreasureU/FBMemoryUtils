ad10e93bf4c6eb102a910274cb8e277eaec19d95
上海-程剑锋 <chengjianfeng@jd.com>
2017-03-24 09:49:33 +0800

    初始化工程提交

---
+++ FBMemoryUtils/FBMemoryUtils.h
@@ -0,0 +1,54 @@
+//
+//  FBMemoryUtils.h
+//  FBMemoryUtils
+//
+//  Created by ChengJianFeng on 2017/3/23.
+//  Copyright © 2017年 ChengJianFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBMemoryUtils.
+FOUNDATION_EXPORT double FBMemoryUtilsVersionNumber;
+
+//! Project version string for FBMemoryUtils.
+FOUNDATION_EXPORT const unsigned char FBMemoryUtilsVersionString[];
+
+#import <FBMemoryUtils/FBStandardGraphEdgeFilters.h>
+#import <FBMemoryUtils/FBObjectiveCObject.h>
+#import <FBMemoryUtils/FBObjectiveCNSCFTimer.h>
+#import <FBMemoryUtils/FBRetainCycleDetector.h>
+#import <FBMemoryUtils/FBObjectiveCGraphElement.h>
+#import <FBMemoryUtils/FBObjectGraphConfiguration.h>
+#import <FBMemoryUtils/FBObjectiveCBlock.h>
+#import <FBMemoryUtils/FBAssociationManager.h>
+#import <FBMemoryUtils/FBSingleRetainCycleViewController.h>
+#import <FBMemoryUtils/FBRetainCyclePresenter.h>
+#import <FBMemoryUtils/FBRetainCycleAnalysisCache.h>
+#import <FBMemoryUtils/FBAllocationTrackerSummary.h>
+#import <FBMemoryUtils/FBMemoryProfilerTextField.h>
+#import <FBMemoryUtils/FBMemoryProfiler.h>
+#import <FBMemoryUtils/FBMemoryProfilerPresenting.h>
+#import <FBMemoryUtils/FBMemoryProfilerMovableViewController.h>
+#import <FBMemoryUtils/FBMemoryProfilerContainerViewController.h>
+#import <FBMemoryUtils/FBMemoryProfilerMathUtils.h>
+#import <FBMemoryUtils/FBMemoryProfilerViewController.h>
+#import <FBMemoryUtils/FBMemoryProfilerDataSource.h>
+#import <FBMemoryUtils/FBMemoryProfilerWindow.h>
+#import <FBMemoryUtils/FBMemoryProfilerFloatingButtonController.h>
+#import <FBMemoryUtils/FBMemoryProfilerDeviceUtils.h>
+#import <FBMemoryUtils/FBMemoryProfilerSectionHeaderDelegate.h>
+#import <FBMemoryUtils/FBMemoryProfilerWindowTouchesHandling.h>
+#import <FBMemoryUtils/FBMemoryProfilerSegmentedControl.h>
+#import <FBMemoryUtils/FBMemoryProfilerView.h>
+#import <FBMemoryUtils/FBMemoryProfilerGenerationsSectionHeaderView.h>
+#import <FBMemoryUtils/FBMemoryProfilerOptions.h>
+#import <FBMemoryUtils/FBMemoryProfilerPluggable.h>
+#import <FBMemoryUtils/FBMemoryProfilerPresentationModeDelegate.h>
+#import <FBMemoryUtils/FBAllocationTrackerManager.h>
+#import <FBMemoryUtils/FBAllocationTracker.h>
+#import <FBMemoryUtils/FBAllocationTrackerDefines.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBMemoryUtils/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# FBMemoryUtils
-对FB的内存分析三件套工具提供了framework版本
+介绍：
+FBMemoryUtils是FBMemoryProfiles的静态库，打出来的包是一个framework，mach-o Type是Static Library。
+官方库[FBMemoryProfiles](https://github.com/facebook/FBMemoryProfiler)地址。
+
+
+内容：
+内含FBMemoryProfiles、FBAllocationTracker、FBRetainCycleDetector三个库的代码
+FBAllocationTracker：是一个对象生成与生存期检测工具
+FBRetainCycleDetector：是一个循环引用监测工具
+FBMemoryProfiles：对上面两种功能进行了集合，并且提供可视化界面、插件功能以及筛选过滤功能。
+
+使用说明：
+1.选中CrossGenerate target，然后直接Build即可。（Debug/Release模式将会决定打包配置，并且取决于CrossGenerate Scheme的配置）
+2.Build完成后，会自动合成集合库，并且open集合库所在的文件夹
+3.将打出的包赋值到工程目录合适的地方，并引入工程
+4.在Link Binary With Libraries中添加：UIKit、CoreGraphics、Foundation
+4.在工程中需要使用时，在该文件中“#import <FBMemoryUtils/FBMemoryUtils>”即可。
+
+代码内使用简单示例：
+1.建议在main中启动对象生成追踪
+	[FBAssociationManager hook];
+	[[FBAllocationTrackerManager sharedManager] startTrackingAllocations];
+	[[FBAllocationTrackerManager sharedManager] enableGenerations];
+
+2.建议在AppDelegate中启动内存分析：
+	self.memoryProfiler = [[FBMemoryProfiler alloc] init];
+	[self.memoryProfiler enable];
